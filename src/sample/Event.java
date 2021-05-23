89535d5ba138f73bc1d16e99870b02b99cfa279d
thesunsavior <73487076+thesunsavior@users.noreply.github.com>
2021-05-23 11:10:53 +0700

    Update Event.java

--- src/sample/Event.java
+++ src/sample/Event.java
@@ -6,70 +6,6 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
-class Date {
-    /**
-     *  use to format a date with
-     *  dd/mm/yy and time: hh/mm
-     * @params Sdate : Integer
-     *              formart the date
-     *              The format storing is as follow:
-     *              Sdate  = {dd/mm/yy/hh/mm}
-     *              for example : 18:15 on 21/05/2021 would be formated
-     *              Sdate = {21,5,2021,18,15}
-     *
-     * @param Stime : Integer
-     *              denote  time
-     *              format  {hh,mm}
-     *  */
-    int Sdate[];
-    int Stime[];
-    Date  (int dd, int mm ,int yy, int hour ,int minute){
-        /**
-         * Format for string Sdate
-         * dd mm yy hour minute
-         */
-
-        this.Sdate = new int[]{dd, mm, yy};
-        this.Stime = new int[]{hour, minute};
-    }
-
-    static int compareDate (int th [],int [] o){
-        /**
-         * Compare two date array
-         * th: integer
-         *      represent "Our" date
-         * o: integer
-         *      represent the other date
-         */
-        for (int i = 2 ; i >= 0 ; i-- )
-            if (th[i] > o[i]) return 1 ;
-            else
-            if (th[i] < o[i]) return -1;
-        return 0;
-    }
-
-    static int  compareTime (int th[], int [] o ){
-        /**
-         * Compare two time array
-         * th: integer
-         *      represent "Our" date
-         * o: integer
-         *      represent the other date
-         */
-        for (int i = 0 ; i < 2; i++)
-            if (th[i] < o[i])
-                return -1;
-            else if (th[i] < o[i] )
-                return 1;
-        return 0 ;
-    }
-    static boolean isDuring (Date start, Date end, Date ck){
-        if (ck.Sdate[2] < start.Sdate[2] || ck.Sdate[2] > end.Sdate[2]) return false;
-        if (ck.Sdate[1] < start.Sdate[1] || ck.Sdate[1] > end.Sdate[1]) return false;
-        if (ck.Sdate[0] < start.Sdate[0] || ck.Sdate[1]  > end.Sdate[1]) return false ;
-        return true;
-    }
-}
 
 public class Event{
     /**
@@ -120,19 +56,19 @@ public class Event{
     }
 }
 
-//class Course extends Event {
-//    //ArrayList<String> instructors;
-//    int credit;
-//    String course_id;
-//
-////    Course(String title, String description, String startTime, String endTime,
-////           String startDate, String endDate, ArrayList<Integer> recur,
-////           int credit, String course_id) {
-////        //super(title, description, startTime, endTime, startDate, endDate, recur);
-////        //this.instructors = instructors;
-////        this.credit = credit;
-////        this.course_id = course_id;
-//    }
+class Course extends Event {
+    //ArrayList<String> instructors;
+    int credit;
+    String course_id,instructors;
+
+    Course(String title, int Shour,int Sminute , int Ehour, int Eminute,
+           int Sdd,int Smm,int Syy,int Edd,int Emm ,int Eyy,
+           int credit, String course_id) {
+        super(new Date (Sdd,Smm,Syy,Shour,Sminute),new Date (Edd,Emm,Eyy,Ehour,Eminute),title,false,true,Color.BLUE);
+        this.instructors = instructors;
+        this.credit = credit;
+        this.course_id = course_id;
+    }
 
     static int  compareTime (int th[], int [] o ){
         /**
@@ -147,12 +83,12 @@ public class Event{
                 return -1;
             else if (th[i] < o[i] )
                 return 1;
-            return 0 ;
+        return 0 ;
     }
     static boolean isDuring (Date start, Date end, Date ck){
-         if (ck.Sdate[2] < start.Sdate[2] || ck.Sdate[2] > end.Sdate[2]) return false;
-         if (ck.Sdate[1] < start.Sdate[1] || ck.Sdate[1] > end.Sdate[1]) return false;
-         if (ck.Sdate[0] < start.Sdate[0] || ck.Sdate[1]  > end.Sdate[1]) return false ;
-         return true;
+        if (ck.Sdate[2] < start.Sdate[2] || ck.Sdate[2] > end.Sdate[2]) return false;
+        if (ck.Sdate[1] < start.Sdate[1] || ck.Sdate[1] > end.Sdate[1]) return false;
+        if (ck.Sdate[0] < start.Sdate[0] || ck.Sdate[1]  > end.Sdate[1]) return false ;
+        return true;
     }
 }
